c22bc650d003a4250a0a7b426eb79cd5b66f7381
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-11-13 21:40:55 -0600

    Collection setup to handle CRUD

---
+++ src/models/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const { Sequelize, DataTypes } = require('sequelize');
+require('dotenv').config();
+const user = require('./User');
+const Collection = require('./Collection');
+
+
